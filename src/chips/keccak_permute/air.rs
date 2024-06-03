b73685c5d57ed290a4c625f1270070f3692fe90e
Ayush Shukla <shuklaayush247@gmail.com>
2024-06-03 13:14:31 +0200

    fix: remove hardcoded range

--- src/chips/keccak_permute/air.rs
+++ src/chips/keccak_permute/air.rs
@@ -1,11 +1,11 @@
 use core::borrow::Borrow;
 use p3_air::{Air, AirBuilder, BaseAir};
-use p3_air_util::builders::SubAirBuilder;
+use p3_air_util::builders::SubRangeAirBuilder;
 use p3_matrix::Matrix;
 
 use super::columns::KeccakPermuteCols;
 use super::KeccakPermuteChip;
-use crate::airs::keccak::{KeccakAir, NUM_KECCAK_COLS, NUM_ROUNDS};
+use crate::airs::keccak::{KeccakAir, NUM_ROUNDS};
 
 impl<F> BaseAir<F> for KeccakPermuteChip {
     fn width(&self) -> usize {
@@ -19,6 +19,8 @@ impl<AB: AirBuilder> Air<AB> for KeccakPermuteChip {
         let local = main.row_slice(0);
         let local: &KeccakPermuteCols<AB::Var> = (*local).borrow();
 
+        let col_map = KeccakPermuteCols::<AB::Var>::col_map();
+
         builder.assert_bool(local.is_real);
         builder.assert_eq(
             local.is_real * local.keccak.step_flags[0],
@@ -31,8 +33,7 @@ impl<AB: AirBuilder> Air<AB> for KeccakPermuteChip {
         );
 
         let keccak_air = KeccakAir {};
-        let mut sub_builder =
-            SubAirBuilder::<AB, KeccakAir, AB::Var>::new(builder, 0..NUM_KECCAK_COLS);
+        let mut sub_builder = SubRangeAirBuilder::new_main(builder, col_map.keccak.range());
         keccak_air.eval(&mut sub_builder);
     }
 }
